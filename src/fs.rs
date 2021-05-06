3fc136a2bbd0855acb9cd09ae5626cb09e606600
Jason Wang <wangborong@cdjrlc.com>
2021-05-06 09:58:47 +0800

    Update fs command
    * enable rg command options
    * auto split search patterns and options

--- src/fs.rs
+++ src/fs.rs
@@ -5,13 +5,26 @@ use crate::shell;
 
 pub fn run(args: &Vec<String>) {
     if args.len() < 1 {
-        eprintln!("Usage: fs <search patterns>");
+        eprintln!("Usage: fs [options] <search patterns>");
         exit(1);
     }
 
     let (_, h) = term_size::dimensions().unwrap();
-    let args_str = args.join(" ");
-    let file_pos_cmd = format!(r#"rg --color=always -n "{}" | fzf -e --tac -0 -1 --color=bg+:24 --cycle --min-height=20 -d ':' --preview="echo '\033[1;32m  {{1}}\033[0m'; fspreview {{}} {}" --preview-window=right:60% | gawk -F':' '{{printf "%s +%s", $1, $2}}'"#, args_str, h);
+
+    // Try to split search strings and options
+    let mut sstrs: Vec<&str> = Vec::new();
+    let mut opts: Vec<&str> = Vec::new();
+    for s in args {
+        if '-' as u8 != s.as_bytes()[0] {
+            sstrs.push(s);
+        } else {
+            opts.push(s);
+        }
+    }
+
+    let sstr = sstrs.join(" ");
+    let opt = opts.join(" ");
+    let file_pos_cmd = format!(r#"rg --color=always -n {} "{}" | fzf -e --tac -0 -1 --color=bg+:24 --cycle --min-height=20 -d ':' --preview="echo '\033[1;32m  {{1}}\033[0m'; fspreview {{}} {}" --preview-window=right:60% | gawk -F':' '{{printf "%s +%s", $1, $2}}'"#, opt, sstr, h);
     let file_pos_out = shell::run_with_out(&file_pos_cmd);
     if file_pos_out.stdout != "" {
         let edit_file_cmd = "nvim ".to_owned() + &file_pos_out.stdout;
