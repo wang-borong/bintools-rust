b243c6e5ddd80698834267e5261f1aec3ede45fc
Jason Wang <1724555125@qq.com>
2021-05-07 20:37:27 +0800

    Fix fzf ansi colors in some systems

--- src/fs.rs
+++ src/fs.rs
@@ -24,7 +24,7 @@ pub fn run(args: &Vec<String>) {
 
     let sstr = sstrs.join(" ");
     let opt = opts.join(" ");
-    let file_pos_cmd = format!(r#"rg --color=always -n {} "{}" | fzf -e --tac -0 -1 --color=bg+:24 --cycle --min-height=20 -d ':' --preview="echo '\033[1;32m  {{1}}\033[0m'; fspreview {{}} {}" --preview-window=right:60% | gawk -F':' '{{printf "%s +%s", $1, $2}}'"#, opt, sstr, h);
+    let file_pos_cmd = format!(r#"rg --color=always -n {} "{}" | fzf --ansi -e --tac -0 -1 --cycle --min-height=20 -d ':' --preview="echo '\033[1;32m  {{1}}\033[0m'; fspreview {{}} {}" --preview-window=right:60% | gawk -F':' '{{printf "%s +%s", $1, $2}}'"#, opt, sstr, h);
     let file_pos_out = shell::run_with_out(&file_pos_cmd);
     if file_pos_out.stdout != "" {
         let edit_file_cmd = "nvim ".to_owned() + &file_pos_out.stdout;
