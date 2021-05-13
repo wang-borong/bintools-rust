15dbbbff26e6d55a378e1bb921e14c654cd397e2
Jason Wang <1724555125@qq.com>
2021-05-13 19:34:10 +0800

    Remove duplicated function

--- src/arg.rs
+++ src/arg.rs
@@ -1,20 +1,9 @@
 use std::env;
-use std::fs;
 use std::process::exit;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
-use crate::sha;
 use crate::shell;
-
-fn get_ignore_path(p: &str) -> PathBuf {
-    let home = env::var("HOME").unwrap();
-    let ignpath = PathBuf::from(home).join(".cache/arg-ignore");
-    if !ignpath.as_path().exists() {
-        fs::create_dir(&ignpath).unwrap();
-    }
-    let cwdh = &sha::sha256(p.as_bytes())[..8];
-    ignpath.join(cwdh)
-}
+use crate::rgignore::get_ignore_path;
 
 pub fn run(arg0: &str, args: &Vec<String>) {
     if args.len() < 1 {
